093dc77f8185554a96d481a9026187d82bb49fd3
Jonathan Cain <jcainuk@gmail.com>
2023-09-14 21:24:52 +0100

    completes delete and update controllers

--- Backend/routes/workouts.js
+++ Backend/routes/workouts.js
@@ -2,7 +2,9 @@ const express = require("express");
 const {
   createWorkout,
   getWorkouts,
-  getWorkout
+  getWorkout,
+  deleteWorkout,
+  updateWorkout
 } = require("../controllers/workoutController");
 
 const router = express.Router();
@@ -17,13 +19,9 @@ router.get("/:id", getWorkout);
 router.post("/", createWorkout);
 
 // DELETE a new workout
-router.delete("/:id", (req, res) => {
-  res.json({ msg: "DELETE a workout" });
-});
+router.delete("/:id", deleteWorkout);
 
 // PATCH a new workout
-router.patch("/:id", (req, res) => {
-  res.json({ msg: "PATCH a workout" });
-});
+router.patch("/:id", updateWorkout);
 
 module.exports = router;
